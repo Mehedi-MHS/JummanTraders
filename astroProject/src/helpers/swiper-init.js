92825ef2117bb58b4d4dd12d11bb7d5ff772bb20
Mehedi-MHS <mehedishuvo685638@gmail.com>
2024-09-07 16:31:56 +0600

    added swiperjs

---
+++ astroProject/src/helpers/swiper-init.js
@@ -0,0 +1,43 @@
+// src/scripts/swiper-init.js
+/*
+import Swiper from "swiper/bundle";
+import "swiper/swiper-bundle.min.css";
+
+document.addEventListener("DOMContentLoaded", function () {
+  new Swiper(".swiper", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+});
+
+*/
+import Swiper from 'swiper';
+import { Navigation, Pagination,Autoplay } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+
+document.addEventListener( 'DOMContentLoaded', function() {
+    const swiper = new Swiper('.swiper', {
+        modules: [Navigation, Pagination,Autoplay],
+        loop:true,
+        speed:400,
+        autoplay:{
+          delay:3000,
+          disableOnInteraction:false
+        },
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        }
+      });
+});
