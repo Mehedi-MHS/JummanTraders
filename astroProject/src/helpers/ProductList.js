c37c71334a3dfcccd4d3cd35816bc4a995b762a6
Mehedi-MHS <mehedishuvo685638@gmail.com>
2024-09-09 19:47:00 +0600

    added banner1

---
+++ astroProject/src/helpers/ProductList.js
@@ -0,0 +1,29 @@
+const Products = [
+    {
+        id:1,
+     name:"Unnamed",
+      url:"/images/products/product1.png",
+        price:500
+    },
+    {
+        id:2,
+     name:"Unnamed",
+      url:"/images/products/product1.png",
+        price:500
+    },
+    {
+        id:3,
+     name:"Unnamed",
+      url:"/images/products/product3.png",
+        price:500
+    },
+    {
+        id:4,
+     name:"Unnamed",
+      url:"/images/products/product4.png",
+        price:500
+    },
+
+]
+
+export default Products;
