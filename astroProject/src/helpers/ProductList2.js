fb1d9160b6b48567ae597b33f82c180cadf1fe36
Mehedi-MHS <mehedishuvo685638@gmail.com>
2024-09-11 19:54:57 +0600

    added services page

---
+++ astroProject/src/helpers/ProductList2.js
@@ -0,0 +1,16 @@
+const Products2 = [
+    { id: 1, name: "Unnamed", url: "/images/products/adidas1.jpeg", price: 500 },
+    { id: 2, name: "Unnamed", url: "/images/products/adidas2.jpeg", price: 500 },
+    { id: 3, name: "Unnamed", url: "/images/products/adidas3.jpeg", price: 500 },
+    { id: 4, name: "Unnamed", url: "/images/products/adidas4.jpeg", price: 500 },
+    { id: 5, name: "Unnamed", url: "/images/products/adidas5.jpeg", price: 500 },
+    { id: 6, name: "Unnamed", url: "/images/products/adidas6.jpeg", price: 500 },
+    { id: 7, name: "Unnamed", url: "/images/products/adidas7.jpeg", price: 500 },
+    { id: 8, name: "Unnamed", url: "/images/products/adidas8.jpeg", price: 500 },
+    { id: 9, name: "Unnamed", url: "/images/products/nike1.jpeg", price: 500 },
+    { id: 10, name: "Unnamed", url: "/images/products/nike2.jpeg", price: 500 },
+    { id: 11, name: "Unnamed", url: "/images/products/nike3.jpeg", price: 500 },
+
+  ];
+
+export default Products2;
