0103cd090e8827b9e6319c8cb245e9a17b0d91dd
Mehedi-MHS <mehedishuvo685638@gmail.com>
2024-09-15 18:43:59 +0600

    about page update

--- astroProject/src/helpers/ProductList2.js
+++ astroProject/src/helpers/ProductList2.js
@@ -1,15 +1,15 @@
 const Products2 = [
-    { id: 1, name: "Unnamed", url: "/images/products/adidas1.jpeg", price: 500 },
-    { id: 2, name: "Unnamed", url: "/images/products/adidas2.jpeg", price: 500 },
-    { id: 3, name: "Unnamed", url: "/images/products/adidas3.jpeg", price: 500 },
-    { id: 4, name: "Unnamed", url: "/images/products/adidas4.jpeg", price: 500 },
-    { id: 5, name: "Unnamed", url: "/images/products/adidas5.jpeg", price: 500 },
-    { id: 6, name: "Unnamed", url: "/images/products/adidas6.jpeg", price: 500 },
-    { id: 7, name: "Unnamed", url: "/images/products/adidas7.jpeg", price: 500 },
-    { id: 8, name: "Unnamed", url: "/images/products/adidas8.jpeg", price: 500 },
-    { id: 9, name: "Unnamed", url: "/images/products/nike1.jpeg", price: 500 },
-    { id: 10, name: "Unnamed", url: "/images/products/nike2.jpeg", price: 500 },
-    { id: 11, name: "Unnamed", url: "/images/products/nike3.jpeg", price: 500 },
+    { id: 1, name: "Adidas Sport's Shoe", url: "/images/products/adidas1.jpeg" },
+    { id: 2, name: "Adidas Sport's Shoe", url: "/images/products/adidas2.jpeg" },
+    { id: 3, name: "Adidas Sport's Shoe", url: "/images/products/adidas3.jpeg" },
+    { id: 4, name: "Adidas Sport's Shoe", url: "/images/products/adidas4.jpeg" },
+    { id: 5, name: "Adidas Sport's Shoe", url: "/images/products/adidas5.jpeg" },
+    { id: 6, name: "Adidas Sport's Shoe", url: "/images/products/adidas6.jpeg" },
+    { id: 7, name: "Adidas Sport's Shoe", url: "/images/products/adidas7.jpeg" },
+    { id: 8, name: "Adidas Sport's Shoe", url: "/images/products/adidas8.jpeg" },
+    { id: 9, name: "Nike Sport's Shoe", url: "/images/products/nike1.jpeg" },
+    { id: 10, name: "Nike Sport's Shoe", url: "/images/products/nike2.jpeg" },
+    { id: 11, name: "Nike Sport's Shoe", url: "/images/products/nike3.jpeg" },
 
   ];
 
