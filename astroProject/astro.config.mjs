279c08c221a94dccfd046ca9ab160bbbdba02a45
Mehedi-MHS <mehedishuvo685638@gmail.com>
2024-09-17 21:00:28 +0600

    added site

--- astroProject/astro.config.mjs
+++ astroProject/astro.config.mjs
@@ -3,5 +3,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     base:'/',
-    outDir:'public'
+    outDir:'public',
+    site:"https://jummantraders.com"
 });
